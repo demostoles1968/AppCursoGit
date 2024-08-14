8694ee26a74f30d915a4c8f79ce6936a75c546a0
demostoles1968 <42360000+demostoles1968@users.noreply.github.com>
2024-08-14 21:44:20 +0200

    Update Title GIT

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      home: MyHomePage(title: 'Curso Git y GitHub en local'),
+      home: MyHomePage(title: 'Curso Git y GitHub en Git'),
     );
   }
 }
